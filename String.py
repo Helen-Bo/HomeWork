42a51128246d5649fb9cae381ee0bf29b86bd0b3
Elena <bohatchuk.o@gmail.com>
2021-12-05 15:21:50 +0200

    1

---
+++ String.py
@@ -0,0 +1,18 @@
+# Пусть пользователь вводит строку искомое слово и слово-замену
+# заменить в строке искомое слово на слово-замену и вывески колличество замен
+# пример:
+# aaa bbb ccc ddd bbb rrr eee ddd
+# bbb
+# ccc
+# вывод:
+# aaa ccc ccc ddd ccc rrr eee ddd
+# 2
+text = input()
+
+old = input('Введите слово,которое хотите заменить - ')
+new = input('Введите слово для замены - ')
+
+print(text)
+text = text.replace(old, new)
+print(text)
+print(text.count(new))
