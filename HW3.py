413d2db8232b2a97229b3d8b3380afa93e459983
Elena <bohatchuk.o@gmail.com>
2021-12-09 00:51:59 +0200

    upd

--- HW3.py
+++ HW3.py
@@ -1,3 +1,25 @@
-num1 = input('Введите первое число -')
-operator = input('Введите оператор')
-num2 = input('Введите первое число -')ж
+actions = []
+element = '2'
+while element != '=':
+    element = input()
+    if element == '=':
+        break
+    actions.append(element)
+
+
+operator = '+'
+result = 0
+for i in actions:
+    if i.isdigit():
+        i = int(i)
+        if operator == '+':
+            result = result + i
+        elif operator == '-':
+            result = result - i
+        elif operator == '*':
+            result = result * i
+        else:
+            result = result / i
+    else:
+        operator = i
+print(result)
