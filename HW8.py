4870536e431dd3fc18eaf216cd373743efaeda61
Elena <bohatchuk.o@gmail.com>
2021-12-11 17:45:27 +0200

    upd1

--- HW8.py
+++ HW8.py
@@ -3,23 +3,17 @@
 # 1 2 3 2 4 5 3 -> 2
 # 2 3 5 3 5 6 -> 3
 import re
-import collections
+
 
 list_a = input()
 
-
-
 element = re.findall(r'[^\b,;:\-\'\"\+@#|\.()%\$!\s]', list_a)
-# print(element)
+
 new_list = []
 for x in element:
     if element.count(x) > 1:
         new_list.append(x)
-
+repeating_elements = set(new_list)
+print(repeating_elements)
 
 print(f'{list_a} -> {new_list[0]}')
-
-#y = collections.Counter(element).most_common(1)
-
-#print(y)
-#print(f'{list_a} -> {y}')
