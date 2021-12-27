667d423eb4e0f4291e69a977f9d05b906e1a5302
Elena <bohatchuk.o@gmail.com>
2021-12-27 22:25:20 +0200

    upd

--- hw14.py
+++ hw14.py
@@ -1,22 +1,15 @@
-first_number = input('Input first value ')
-second_number = input('Input second value ')
+first_number = int(input('Input first value '))
+second_number = int(input('Input second value '))
 
 
 def greatest_common_factor(num1, num2):
     while num1 != num2:
-        if num1.isdigit() and num2.isdigit():
-            num1 = int(num1)
-            num2 = int(num2)
-            if num1 > num2:
-                num1 = num1 - num2
-            else:
-                num2 = num2 - num1
+        if num1 > num2:
+            num1 = num1 - num2
         else:
-            print('Unknown value')
-            break
+            num2 = num2 - num1
     return num1
 
 
 GCF = greatest_common_factor(first_number, second_number)
 print(f'Greatest common factor {first_number} and {second_number} is equal to {GCF}')
-
