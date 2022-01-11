ad9cb12e80c1443af46bf7c348a230d1f2039aeb
Elena <bohatchuk.o@gmail.com>
2022-01-12 01:41:28 +0200

    upd

---
+++ hw18.py
@@ -0,0 +1,51 @@
+class MyTemperature:
+
+    dict = {'K': 273.15, 'F': 5/9, 'C': 1}
+
+    def __init__(self, input_temp, measure):
+        self.Kc = input_temp + MyTemperature.dict[measure]
+        self.Kf = MyTemperature.kelvin_to_fahrenheit(input_temp, measure)
+        self.Fc = MyTemperature.celsius_to_fahrenheit(input_temp, measure)
+        self.Fk = MyTemperature.kelvin_to_fahrenheit(input_temp, measure)
+        self.Ck = MyTemperature.celsius_to_kelvin(input_temp, measure)
+        self.Cf = MyTemperature.celsius_to_fahrenheit(input_temp, measure)
+
+    @staticmethod
+    def kelvin_to_celsius(my_temp, measure):
+        k_to_c = my_temp - MyTemperature.dict['K'] * MyTemperature.dict[measure]
+        return k_to_c
+
+    @staticmethod
+    def kelvin_to_fahrenheit(my_temp, measure):
+        k_to_f = (my_temp - MyTemperature.dict['K']) * (MyTemperature.dict[measure]**(-1)) + 32
+        return k_to_f
+
+    @staticmethod
+    def celsius_to_kelvin(my_temp, measure):
+        c_to_k = my_temp + MyTemperature.dict[measure]
+        return c_to_k
+
+    @staticmethod
+    def celsius_to_fahrenheit(my_temp, measure):
+        c_to_f = (my_temp * (MyTemperature.dict[measure]**(-1))) + 32
+        return c_to_f
+
+    @staticmethod
+    def fahrenheit_to_kelvin(my_temp, measure):
+        f_to_k = (my_temp - 32) * MyTemperature.dict['F'] + MyTemperature.dict[measure]
+        return f_to_k
+
+    @staticmethod
+    def fahrenheit_to_celsius(my_temp, measure):
+        f_to_c = ((my_temp - 32) * MyTemperature.dict['F']) * MyTemperature.dict[measure]
+        return f_to_c
+
+
+print(MyTemperature.celsius_to_fahrenheit(10, 'F'))
+print(MyTemperature.celsius_to_kelvin(10, 'K'))
+
+print(MyTemperature.kelvin_to_fahrenheit(10, 'F'))
+print(MyTemperature.kelvin_to_celsius(10, 'C'))
+
+print(MyTemperature.fahrenheit_to_kelvin(10, 'K'))
+print(MyTemperature.fahrenheit_to_celsius(10, 'C'))
