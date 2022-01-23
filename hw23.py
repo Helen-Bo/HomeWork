4e0482d79a291dec4753f1a36305936abdc2cf78
Elena <bohatchuk.o@gmail.com>
2022-01-23 10:54:40 +0200

    upd

---
+++ hw23.py
@@ -0,0 +1,14 @@
+import os
+
+
+def get_files_info(*args):
+    for root, dirs, files in os.walk(dir_name):
+        for file_name in files:
+            abs_name = os.path.join(root, file_name)
+        yield abs_name, os.stat(abs_name)
+
+
+dir_name = r'.'
+files_sorted_by_size = sorted(get_files_info(dir_name), reverse=True, key=lambda x: x[1].st_size)
+print('\n'.join(str(value) for value in files_sorted_by_size[:10]))
+
