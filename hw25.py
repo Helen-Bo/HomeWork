b43de12cb92db1a4bb16ac30b76d1c64008feacb
Elena <bohatchuk.o@gmail.com>
2022-02-05 00:58:30 +0200

    upd

--- hw25.py
+++ hw25.py
@@ -19,51 +19,51 @@ def get_text(url):
 def get_horoscope(message):
     zodiac_map = {'aries': '♈', 'taurus': '♉', 'gemini': '♊', 'cancer': '♋', 'lion': '♌', 'virgo': '♍', 'libra': '♎',
                   'scorpio': '♏', 'sagittarius': '♐', 'capricorn': '♑', 'aquarius': '♒', 'pisces': '♓'}
-
-    if message == 'Овен':
-        zodiac_icon = zodiac_map['aries']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/aries/today.html')
-    elif message == 'Телец':
-        zodiac_icon = zodiac_map['taurus']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/taurus/today.html')
-    elif message == 'Близнецы':
-        zodiac_icon = zodiac_map['gemini']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/gemini/today.html')
-    elif message == 'Рак':
-        zodiac_icon = zodiac_map['cancer']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/cancer/today.html')
-    elif message == 'Лев':
-        zodiac_icon = zodiac_map['lion']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/lion/today.html')
-    elif message == 'Дева':
-        zodiac_icon = zodiac_map['virgo']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/virgo/today.html')
-    elif message == 'Весы':
-        zodiac_icon = zodiac_map['libra']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/libra/today.html')
-    elif message == 'Скорпион':
-        zodiac_icon = zodiac_map['scorpio']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/scorpio/today.html')
-    elif message == 'Стерец':
-        zodiac_icon = zodiac_map['sagittarius']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/sagittarius/today.html')
-    elif message == 'Козерог':
-        zodiac_icon = zodiac_map['capricorn']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/capricorn/today.html')
-    elif message == 'Водолей':
-        zodiac_icon = zodiac_map['aquarius']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/aquarius/today.html')
-    elif message == 'Рыбы':
-        zodiac_icon = zodiac_map['pisces']
-        prediction = get_text('https://orakul.com/horoscope/astrologic/general/pisces/today.html')
-    # else:
-    #     # zodiac_icon = 'none'
-    #     prediction = 'Введите знак зодиака киррилицей  первой заглавной буквой'
-    your_prediction = f''' Ежедневный голоскоп для {message}:
-    {zodiac_icon}
-{prediction}
-                        '''
-    return your_prediction
+    zodiac_sign = ['Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион', 'Стрелец', 'Козерог', 'Водолей' , 'Рыбы']
+    if message in zodiac_sign:
+        if message == 'Овен':
+            zodiac_icon = zodiac_map['aries']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/aries/today.html')
+        elif message == 'Телец':
+            zodiac_icon = zodiac_map['taurus']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/taurus/today.html')
+        elif message == 'Близнецы':
+            zodiac_icon = zodiac_map['gemini']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/gemini/today.html')
+        elif message == 'Рак':
+            zodiac_icon = zodiac_map['cancer']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/cancer/today.html')
+        elif message == 'Лев':
+            zodiac_icon = zodiac_map['lion']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/lion/today.html')
+        elif message == 'Дева':
+            zodiac_icon = zodiac_map['virgo']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/virgo/today.html')
+        elif message == 'Весы':
+            zodiac_icon = zodiac_map['libra']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/libra/today.html')
+        elif message == 'Скорпион':
+            zodiac_icon = zodiac_map['scorpio']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/scorpio/today.html')
+        elif message == 'Стрелец':
+            zodiac_icon = zodiac_map['sagittarius']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/sagittarius/today.html')
+        elif message == 'Козерог':
+            zodiac_icon = zodiac_map['capricorn']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/capricorn/today.html')
+        elif message == 'Водолей':
+            zodiac_icon = zodiac_map['aquarius']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/aquarius/today.html')
+        elif message == 'Рыбы':
+            zodiac_icon = zodiac_map['pisces']
+            prediction = get_text('https://orakul.com/horoscope/astrologic/general/pisces/today.html')
+        your_prediction = f''' Ежедневный голоскоп для {message}:
+        {zodiac_icon}
+    {prediction}
+                            '''
+        return your_prediction
+    else:
+        return 'Введите знак зодиака кирилицей.\nПервая буква заглавная, остальные сторчные.\n'
 
 
 @bot.message_handler(commands=['start'])
